77b829636fb1c3fd16fa455e3cca402c0dd60a7e
Samurai17jr <aleksandrsolodovnikov228@gmail.com>
2025-12-24 12:23:54 +0300

    add web.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.api.sample import router as sample_router
 from app.api.auth import router as auth_router
 from app.api.roles import router as role_router
 
 app = FastAPI(title="individual_project_template", version="0.0.1")
 
+app.mount("/static", StaticFiles(directory="app/static"), "static")
 app.include_router(sample_router)
 app.include_router(auth_router)
 app.include_router(role_router)
